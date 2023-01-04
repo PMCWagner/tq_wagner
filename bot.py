9709e2442549013040cb5e72e7d116565bf24568
Jolly Mate <111024435+jollymate@users.noreply.github.com>
2023-01-04 16:52:35 +0300

    upd

--- bot.py
+++ bot.py
@@ -1,23 +1,21 @@
-from pyrogram.errors import FloodWait, SlowmodeWait
+from pyrogram.errors import FloodWait, SlowmodeWait, ChatWriteForbidden
 from pyrogram import Client, filters, enums
 import random
 import asyncio
 import time
+import cfg
 
 
-phone = "alice"
-api_id = 25983686
-api_hash = "d49ffa3e2b617c66250b7f4c169d1cb9"
-chance = 7 #шанс ответа
-chance = list(range(chance))
-app = Client(phone, api_id, api_hash)
+chance = list(range(cfg.chance))
+app = Client("alice", cfg.api_id, cfg.api_hash)
 ignorechats = {}
 f = open("words.txt", encoding="utf-8", errors="ignore")
 data = f.read()
 f.close()
+my_id = cfg.my_id
 msgs = [ms for ms in data.split("\n") if ms]
-ignoreusers = [] #игнор пользователей (айдишники через запятую вставлять внутри квадратных скобок)
-chats = [-1001816236019] #игнор чатов (айдишники через запятую вставлять внутри квадратных скобок)
+ignoreusers = cfg.ignoreusers
+chats = cfg.chats
 
 @app.on_message(filters.all)
 async def hello(client, message):
@@ -28,20 +26,20 @@ async def hello(client, message):
     time_now = int(time.time())
     chat_id = message.chat.id
     try:
-        print("Айди пользователя:", message.from_user.id)
-    except:
-        print("Айди пользователя:", message.sender_chat.id)
-    print("Айди чата:", chat_id)
-    try:
         ignorechats[chat_id]
     except KeyError:
         ignorechats[chat_id] = 0
     ch = random.choice(chance)
+    if message.reply_to_message:
+        if message.reply_to_message.from_user:
+            rep_id = message.reply_to_message.from_user.id
+            if rep_id == my_id:
+                ch = 1
     if ch == 1 and not bot and message.from_user.id not in ignoreusers and message.from_user.id > 0 and chat_id not in chats:
         if ignorechats[chat_id] < time_now:
             try:
                 await app.send_chat_action(chat_id, enums.ChatAction.TYPING)
-                await asyncio.sleep(random.randint(1, 3))
+                await asyncio.sleep(random.randint(3, 5))
                 msg = random.choice(msgs)
                 msg_id = message.id
                 await app.send_message(chat_id, msg, reply_to_message_id=msg_id)
@@ -52,6 +50,9 @@ async def hello(client, message):
             except FloodWait as e:
                 print(e)
                 ignorechats[chat_id] = time_now+e.value
+            except ChatWriteForbidden as e:
+                print(e)
+                ignorechats[chat_id] = time_now+3600
 
 
 app.run()
