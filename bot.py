b47d98ecec2522f2f5f57610b1ce103527d8b2f1
Jolly Mate <111024435+jollymate@users.noreply.github.com>
2023-01-14 12:32:41 +0300

    update 14 01
    added logs + fixes

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-from pyrogram.errors import FloodWait, SlowmodeWait, ChatWriteForbidden
+from pyrogram.errors import FloodWait, SlowmodeWait, ChatWriteForbidden, UserBannedInChannel
 from pyrogram import Client, filters, enums
-import random
+import datetime
 import asyncio
+import random
 import time
 import cfg
 
@@ -16,6 +17,16 @@ app = Client(
     system_version=cfg.system_version,
     lang_code=cfg.lang_code
 )
+now = datetime.datetime.now()
+n = str(now).replace(" ", "_").replace(":", "-").split(".")[0]
+log_path = f"logs/log_{n}.txt"
+
+def write_log(info):
+    print(info)
+    f = open(log_path, "at", encoding='utf-8', errors='ignore')
+    f.write(info+"\n")
+    f.close()
+
 ignorechats = {}
 f = open("words.txt", encoding="utf-8", errors="ignore")
 data = f.read()
@@ -27,40 +38,57 @@ chats = cfg.chats
 
 @app.on_message(filters.all)
 async def hello(client, message):
-    try:
-        bot = message.from_user.is_bot
-    except:
-        bot = False
-    time_now = int(time.time())
-    chat_id = message.chat.id
-    try:
-        ignorechats[chat_id]
-    except KeyError:
-        ignorechats[chat_id] = 0
-    ch = random.choice(chance)
-    if message.reply_to_message:
-        if message.reply_to_message.from_user:
-            rep_id = message.reply_to_message.from_user.id
-            if rep_id == my_id:
-                ch = 1
-    if ch == 1 and not bot and message.from_user.id not in ignoreusers and message.from_user.id > 0 and chat_id not in chats:
-        if ignorechats[chat_id] < time_now:
-            try:
-                await app.send_chat_action(chat_id, enums.ChatAction.TYPING)
-                await asyncio.sleep(random.randint(3, 5))
-                msg = random.choice(msgs)
-                msg_id = message.id
-                await app.send_message(chat_id, msg, reply_to_message_id=msg_id)
-                await app.read_chat_history(chat_id)
-            except SlowmodeWait as e:
-                print(e)
-                ignorechats[chat_id] = time_now+e.value
-            except FloodWait as e:
-                print(e)
-                ignorechats[chat_id] = time_now+e.value
-            except ChatWriteForbidden as e:
-                print(e)
-                ignorechats[chat_id] = time_now+3600
+    user = message.from_user
+    if user:
+        try:
+            bot = message.from_user.is_bot
+        except:
+            bot = False
+        time_now = int(time.time())
+        u_id = user.id
+        first_name = user.first_name
+        username = user.username
+        chat_id = message.chat.id
+        msg = message.text
+        try:
+            ignorechats[chat_id]
+        except KeyError:
+            ignorechats[chat_id] = 0
+        ch = random.choice(chance)
+        if chat_id > 0:
+            write_log("*"*50)
+            write_log(f"Айди чата: {chat_id}")
+            write_log(f"От кого: [{u_id}]({username}){first_name}")
+            write_log(f"Сообщение: {msg}")
+            
+        if message.reply_to_message:
+            if message.reply_to_message.from_user:
+                rep_id = message.reply_to_message.from_user.id
+                if rep_id == my_id:
+                    ch = 1
+                    write_log("*"*50)
+                    write_log(f"Айди чата: {chat_id}")
+                    write_log(f"От кого: [{u_id}]({username}){first_name}")
+                    write_log(f"Сообщение: {msg}")
+        if ch == 1 and not bot and message.from_user.id not in ignoreusers and message.from_user.id > 0 and chat_id not in chats:
+            if ignorechats[chat_id] < time_now:
+                try:
+                    await app.send_chat_action(chat_id, enums.ChatAction.TYPING)
+                    await asyncio.sleep(random.randint(3, 5))
+                    msg = random.choice(msgs)
+                    msg_id = message.id
+                    await app.send_message(chat_id, msg, reply_to_message_id=msg_id)
+                    await app.read_chat_history(chat_id)
+                except SlowmodeWait as e:
+                    print(e)
+                    ignorechats[chat_id] = time_now+e.value+2
+                except FloodWait as e:
+                    print(e)
+                    ignorechats[chat_id] = time_now+e.value+2
+                except ChatWriteForbidden:
+                    pass
+                except UserBannedInChannel:
+                    pass
 
 
 app.run()
